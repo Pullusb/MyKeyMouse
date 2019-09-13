15bb9605bb67a78ec0a164688030d87548dd4186
Samuel Bernou <bernou.samuel@gmail.com>
2019-09-13 22:02:02 +0200

    Better prefs, added origin point control

--- MyKeyMouse.py
+++ MyKeyMouse.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "MyKeyMouse",
     "description": "Add 'view selected' and 'view all' actions to mouse buttons 4 and 5",
     "author": "Samuel Bernou",
-    "version": (0, 0, 9),
+    "version": (1, 0, 0),
     "blender": (2, 80, 0),
     "location": "Mouse button 4 (usually 'previous') and 5 (usually 'next') on almost all editors",
     "warning": "",
@@ -27,58 +27,58 @@ def register_keymaps():
     addon_prefs = preferences.addons[__name__].preferences
 
     #mouse 4 and 5 often correspond to previous and next on mouse device
-    if addon_prefs.mkmouse_invert_buttons == True:
-        key_one = "BUTTON4MOUSE"
-        key_two = "BUTTON5MOUSE"
+    key_prev = "BUTTON4MOUSE"#prev
+    key_next = "BUTTON5MOUSE"#next
+
+    if addon_prefs.mkmouse_invert_focus == True:
+        key_focus = "BUTTON4MOUSE"
+        key_home = "BUTTON5MOUSE"
     else:#default
-        key_one = "BUTTON5MOUSE"
-        key_two = "BUTTON4MOUSE"
+        key_focus = "BUTTON5MOUSE"
+        key_home = "BUTTON4MOUSE"
 
     shortcuts_items = [
 
     ##button 4 - focus selection
-    ["3D View", "VIEW_3D", "view3d.view_selected", key_one],
-    ["Graph Editor", "GRAPH_EDITOR", "graph.view_selected", key_one],
-    ["Dopesheet", 'DOPESHEET_EDITOR', "action.view_selected", key_one],
-    ["Sequencer", 'SEQUENCE_EDITOR', "sequencer.view_selected", key_one],
-    ["Node Editor", "NODE_EDITOR", "node.view_selected", key_one],
-    ["NLA Editor", "NLA_EDITOR", "nla.view_selected", key_one],
-    ["Clip Editor", "CLIP_EDITOR", "clip.view_selected", key_one],
-    ["Image", "IMAGE_EDITOR", "image.view_selected", key_one],
-    ["Outliner", "OUTLINER", "outliner.show_active", key_one],
+    ["3D View", "VIEW_3D", "view3d.view_selected", key_focus],
+    ["Graph Editor", "GRAPH_EDITOR", "graph.view_selected", key_focus],
+    ["Dopesheet", 'DOPESHEET_EDITOR', "action.view_selected", key_focus],
+    ["Sequencer", 'SEQUENCE_EDITOR', "sequencer.view_selected", key_focus],
+    ["Node Editor", "NODE_EDITOR", "node.view_selected", key_focus],
+    ["NLA Editor", "NLA_EDITOR", "nla.view_selected", key_focus],
+    ["Clip Editor", "CLIP_EDITOR", "clip.view_selected", key_focus],
+    ["Image", "IMAGE_EDITOR", "image.view_selected", key_focus],
+    ["Outliner", "OUTLINER", "outliner.show_active", key_focus],
 
     ##button 5 - view all
-    ["3D View", "VIEW_3D", "view3d.view_all", key_two],
-    ["Graph Editor", "GRAPH_EDITOR", "graph.view_all", key_two],
-    ["Image", "IMAGE_EDITOR", "image.view_all", key_two],
-    ["Node Editor", "NODE_EDITOR", "node.view_all", key_two],
-    ["Dopesheet", "DOPESHEET_EDITOR", "action.view_all", key_two],
-    ["NLA Editor", "NLA_EDITOR", "nla.view_all", key_two],
-    ["Sequencer", "SEQUENCE_EDITOR", "sequencer.view_all", key_two],
-    ["SequencerPreview", "SEQUENCE_EDITOR", "sequencer.view_all_preview", key_two],
-    ["Clip Editor", "CLIP_EDITOR", "clip.view_all", key_two],
-    ["Clip Graph Editor", "CLIP_EDITOR", "clip.graph_view_all", key_two],
-    ["Clip Dopesheet Editor", "CLIP_EDITOR", "clip.dopesheet_view_all", key_two],
+    ["3D View", "VIEW_3D", "view3d.view_all", key_home],
+    ["Graph Editor", "GRAPH_EDITOR", "graph.view_all", key_home],
+    ["Image", "IMAGE_EDITOR", "image.view_all", key_home],
+    ["Node Editor", "NODE_EDITOR", "node.view_all", key_home],
+    ["Dopesheet", "DOPESHEET_EDITOR", "action.view_all", key_home],
+    ["NLA Editor", "NLA_EDITOR", "nla.view_all", key_home],
+    ["Sequencer", "SEQUENCE_EDITOR", "sequencer.view_all", key_home],
+    ["SequencerPreview", "SEQUENCE_EDITOR", "sequencer.view_all_preview", key_home],
+    ["Clip Editor", "CLIP_EDITOR", "clip.view_all", key_home],
+    ["Clip Graph Editor", "CLIP_EDITOR", "clip.graph_view_all", key_home],
+    ["Clip Dopesheet Editor", "CLIP_EDITOR", "clip.dopesheet_view_all", key_home],
 
-    #not exists anymore
-    # ["Timeline", "TIMELINE", "time.view_all", key_two],
-    #["Logic Editor", "LOGIC_EDITOR", "logic.view_all", key_two],
-
-    #other
-    #["Frames", "EMPTY", "screen.keyframe_jump", key_one, False, False, True]
+    #not exists anymore in 2.8
+    # ["Timeline", "TIMELINE", "time.view_all", key_home],
+    #["Logic Editor", "LOGIC_EDITOR", "logic.view_all", key_home],
 
     ]
 
-    shortcuts_items.append(["3D View", "VIEW_3D", "view3d.localview", key_one, True, False, False])
+    shortcuts_items.append(["3D View", "VIEW_3D", "view3d.localview", key_next, True, False, False])
 
     # Snapping utility with shift (cursor to selection and selection to selected)
-    shortcuts_items.append(["3D View", "VIEW_3D", "view3d.snap_cursor_to_selected", key_two, False, True, False])
-    shortcuts_items.append(["3D View", "VIEW_3D", "view3d.snap_selected_to_cursor", key_one, False, True, False])
+    shortcuts_items.append(["3D View", "VIEW_3D", "view3d.snap_cursor_to_selected", key_prev, False, True, False])
+    shortcuts_items.append(["3D View", "VIEW_3D", "view3d.snap_selected_to_cursor", key_next, False, True, False])
 
 
-    ##3D view > keymap view center pick on mouse (Alt+F) Changed to 'alt + MMB' to match early 2.8 settings
+    ## 2.79 : 3D view > keymap view center pick on mouse (Alt+F) Changed to 'alt + MMB' to match early 2.8 settings
     #if LooseVersion(str(bpy.app.version)) < LooseVersion('(2, 80, 0)'):#only set for 2.79
-    shortcuts_items.append(["3D View", "VIEW_3D", "view3d.view_center_pick", "MIDDLEMOUSE", False, False, True])
+    # shortcuts_items.append(["3D View", "VIEW_3D", "view3d.view_center_pick", "MIDDLEMOUSE", False, False, True])
 
     ## appending all keymap from above list
     addon = bpy.context.window_manager.keyconfigs.addon
@@ -91,16 +91,23 @@ def register_keymaps():
             kmi = km.keymap_items.new(item[2], type = item[3], value = "PRESS")
         addon_keymaps.append(km)
 
+    # Moving origin point utility, cursor/geometry. (hold properties)
+    km = addon.keymaps.new(name = "3D View", space_type = "VIEW_3D")
+    kmi = km.keymap_items.new("object.origin_set", type = key_next, value = "PRESS", ctrl = True, shift = True, alt = True)
+    kmi.properties.type = 'ORIGIN_CURSOR'
+    kmi = km.keymap_items.new("object.origin_set", type = key_prev, value = "PRESS", ctrl = True, shift = True, alt = True)
+    kmi.properties.type = 'ORIGIN_GEOMETRY'
+    addon_keymaps.append(km)
 
-        #special case (because hold properties), combo keyframe jump with alt (button 6 et 7 not working with logitech software...)
-        km = addon.keymaps.new(name = "Frames", space_type = "EMPTY")
-        #kmi = km.keymap_items.new("screen.keyframe_jump", type = "BUTTON6MOUSE", value = "PRESS")#mouse button above 5 aren't recognize on logitech mouse on windaube
-        kmi = km.keymap_items.new("screen.keyframe_jump", type = key_one, value = "PRESS", alt = True)
-        kmi.properties.next = False
-        #kmi = km.keymap_items.new("screen.keyframe_jump", type = "BUTTON7MOUSE", value = "PRESS")#mouse button above 5 aren't recognize on logitech mouse on windaube
-        kmi = km.keymap_items.new("screen.keyframe_jump", type = key_two, value = "PRESS", alt = True)
-        kmi.properties.next = True
-
+    # Combo keyframe jump with alt special case (hold properties). Button 6 et 7 not working with logitech software...
+    km = addon.keymaps.new(name = "Window", space_type = "EMPTY")
+    #kmi = km.keymap_items.new("screen.keyframe_jump", type = "BUTTON6MOUSE", value = "PRESS")
+    kmi = km.keymap_items.new("screen.keyframe_jump", type = key_prev, value = "PRESS", alt = True)
+    kmi.properties.next = False
+    #kmi = km.keymap_items.new("screen.keyframe_jump", type = "BUTTON7MOUSE", value = "PRESS")
+    kmi = km.keymap_items.new("screen.keyframe_jump", type = key_next, value = "PRESS", alt = True)
+    kmi.properties.next = True
+    addon_keymaps.append(km)
 
 
 
@@ -109,8 +116,8 @@ def register_keymaps():
 class My_key_mouse_addon_pref(bpy.types.AddonPreferences):
     bl_idname = __name__
 
-    mkmouse_invert_buttons : bpy.props.BoolProperty(
-        name="Invert buttons everywhere (prev button = view all, next button = view selected)",
+    mkmouse_invert_focus : bpy.props.BoolProperty(
+        name="Invert (prev button = view selected, next button = view all. Save settings and restart Blender to apply changes",
         default=False,
         )
     '''
@@ -127,32 +134,35 @@ class My_key_mouse_addon_pref(bpy.types.AddonPreferences):
     def draw(self, context):
         layout = self.layout
         layout.label(
-            text="In all editor :")
+            text="Focus control (all editor) :")
         layout.label(
-            text="mouse Prev button = view selected")
+            text="mouse Prev button = view all")
         layout.label(
-            text="mouse Next button = view all")
-        layout.label(text="")
+            text="mouse Next button = view selected")
+        # layout.label(text="Customization ")
+        layout.prop(self, "mkmouse_invert_focus")
+        
+        layout.separator()
 
-        layout.label(
-            text="Only in 3D viewport:")
-
+        layout.label(text="Only in 3D viewport:")
         layout.label(text="Ctrl + mouse Next button = Use local view (like numpad slash)")
-        #layout.label(text="Ctrl + mouse Prev button = Use centering view on mouse ")
-
+        #layout.label(text="Ctrl + mouse Prev button = Unnassigned")
         layout.label(text="Cursor Snapping:")
         layout.label(text="Shift + mouse Prev button = 3D cursor to selection")
         layout.label(text="Shift + mouse Next button = selection to 3D cursor")
-        layout.label(text="")
+        layout.label(text="Origin change:")
+        layout.label(text="Ctrl + Shift + alt + mouse Prev button = Origin to geometry")
+        layout.label(text="Ctrl + Shift + alt + mouse Next button = Origin to cursor")
 
-        layout.label(text="Extra:")
-        layout.label(text="Alt + middle mouse button = centering view on mouse (default shortcut in 2.8) as with Alt+F ")
+        # layout.label(text="Alt + middle mouse button = centering view on mouse (default shortcut in 2.8) as with Alt+F ")
+        
+        layout.separator()
+
+        layout.label(text="In all editor:")
         layout.label(text="Alt + mouse Prev button = jump to prev keyframe ")
         layout.label(text="Alt + mouse Next button = jump to next keyframe ")
-        layout.label(text="")
-        layout.label(
-            text="Customization (save settings and restart Blender to apply changes)")
-        layout.prop(self, "mkmouse_invert_buttons")
+    
+
         '''
         layout.label(
             text="Options to swap calls in 3D viewport (accessible with 'ctrl' modifier):")
